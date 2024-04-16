9ffce137c5f2cfeabce899d198b0b684f0208deb
Manjari <97601244+manjshank@users.noreply.github.com>
2024-04-16 17:18:36 +0100

    analysis_marital_status

---
+++ main.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(here)
+
+gss_cat
+gss_cat %>%
+  group_by(marital) %>%
+  summarise(mean_tv_hours = mean(tvhours, na.rm = T))
+tv_hours_table <- gss_cat %>%
+  group_by(marital) %>%
+  summarise(mean_tv_hours = mean(tvhours, na.rm = T))
+
+write_csv(tv_hours_table, here("TV_Hours_By_MaritalStatus.csv") )
+
