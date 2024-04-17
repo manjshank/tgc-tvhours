f33df520af448d0333dd1f81df692b1021e286f6
Manjari <97601244+manjshank@users.noreply.github.com>
2024-04-17 08:35:46 +0100

    change file name

--- main.R
+++ main.R
@@ -6,10 +6,10 @@ gss_cat %>%
   group_by(marital) %>%
   summarise(mean_tv_hours = mean(tvhours, na.rm = T))
 
-tv_hours_table <- gss_cat %>%
+tv_hours_tabulation <- gss_cat %>%
   filter(age <= 30) %>% 
   group_by(marital) %>%
   summarise(mean_tv_hours = mean(tvhours, na.rm = T))
 
-write_csv(tv_hours_table, here("TV_Hours_By_MaritalStatus.csv") )
+write_csv(tv_hours_tabulation, here("TV_Hours_By_MaritalStatus.csv") )
 
